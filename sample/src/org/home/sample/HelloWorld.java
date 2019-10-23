3f6fb03b566e33352e157b4d7680ce4b4be562f0
vijay <vijayrudra@gmail.com>
2019-10-23 13:09:56 -0400

    FIrst commit

--- sample/src/org/home/sample/HelloWorld.java
+++ sample/src/org/home/sample/HelloWorld.java
@@ -2,7 +2,7 @@ package org.home.sample;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Helo World Sample");
 		
 	}
 
